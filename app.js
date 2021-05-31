1311d35fad254a1cf4deb65e5d2f2797f385bdb7
edward_gio <edward_gio@hotmail.com>
2021-05-31 10:53:57 -0500

    Ajuste de puerto

--- app.js
+++ app.js
@@ -48,7 +48,7 @@
 const express = require("express");
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log("Servidor ejecutandose desde puerto", port);
